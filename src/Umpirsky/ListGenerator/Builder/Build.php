7b3d0307081c6ff60ce081f37ba6466cbf83ece7
Fabien Warniez <fabien@warniez.com>
2016-02-13 20:51:23 -0800

    Added Xliff exporter

--- src/Umpirsky/ListGenerator/Builder/Build.php
+++ src/Umpirsky/ListGenerator/Builder/Build.php
@@ -5,7 +5,6 @@ namespace Umpirsky\ListGenerator\Builder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Umpirsky\ListGenerator\Exporter\Iterator as ExporterIterator;
